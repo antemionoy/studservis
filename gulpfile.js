f47862d64fc38adba52b6d8d7da982e5ab7433c9
Anton <antemionoy@gmail.com>
2021-02-07 16:27:59 +0300

    create
    create

---
+++ gulpfile.js
@@ -0,0 +1,221 @@
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    plumber = require('gulp-plumber'),
+    spritesmith = require('gulp.spritesmith'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
+    cssmin = require('gulp-cssmin'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    rimraf = require('rimraf'),
+    cssfont64 = require('gulp-cssfont64'),
+    browserSync = require("browser-sync"),
+    svgSprite = require('gulp-svg-sprite'),
+    svgmin = require('gulp-svgmin'),
+    cheerio = require('gulp-cheerio'),
+    replace = require('gulp-replace'),
+    reload = browserSync.reload;
+
+var path = {
+    build: {
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/',
+        fonts: 'build/fonts/',
+        svg: 'build/svg/'
+    },
+    src: {
+        html: 'src/*.html',
+        js: 'src/js/main.js',
+        style: 'src/style/main.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*',
+        svg: 'src/svg/*.svg'
+    },
+    watch: {
+        html: 'src/**/*.html',
+        js: 'src/js/**/*.js',
+        style: 'src/style/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*',
+        svg: 'src/svg/*.svg'
+    },
+    clean: './build'
+};
+
+var config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: false,
+    host: 'localhost',
+    port: 8080,
+    logPrefix: "frontend"
+};
+
+gulp.task('webserver', function() {
+    browserSync(config);
+});
+
+gulp.task('clean', function(cb) {
+    rimraf(path.clean, cb);
+});
+
+gulp.task('html:build', function() {
+    gulp.src(path.src.html)
+        .pipe(rigger())
+        .pipe(plumber())
+        .pipe(gulp.dest(path.build.html))
+        .pipe(reload({ stream: true }));
+});
+
+gulp.task('js:build', function() {
+    gulp.src(path.src.js)
+        .pipe(rigger())
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({ stream: true }));
+});
+
+gulp.task('style:build', function() {
+    gulp.src(path.src.style)
+        .pipe(plumber())
+        .pipe(prefixer())
+        .pipe(sass({
+            sourceMap: true,
+            errLogToConsole: true
+        }))
+        .pipe(rigger())
+        .pipe(cssmin())
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css))
+        .pipe(reload({ stream: true }));
+});
+
+
+gulp.task('image:build', function() {
+    gulp.src(path.src.img)
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.img))
+        .pipe(reload({ stream: true }));
+});
+
+gulp.task('fonts:build', function() {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts))
+});
+
+gulp.task('svg:build', function() {
+    gulp.src(path.src.svg)
+        .pipe(gulp.dest(path.build.svg))
+});
+
+
+gulp.task('sprite:build', function() {
+    var spriteData =
+        gulp.src('src/img/sprite/*.*') // путь, откуда берем картинки для спрайта
+        .pipe(spritesmith({
+            imgName: 'sprite.png',
+            cssName: 'sprite.css',
+            padding: 15,
+        }));
+
+    spriteData.img.pipe(gulp.dest('./build/img/')); // путь, куда сохраняем картинку
+    spriteData.css.pipe(gulp.dest('./build/css/')); // путь, куда сохраняем стили
+});
+
+
+gulp.task('svgSpriteBuild', function() {
+    return gulp.src('./src/svg/*.svg')
+        // minify svg
+        .pipe(svgmin({
+            js2svg: {
+                pretty: true
+            }
+        }))
+        // remove all fill, style and stroke declarations in out shapes
+        .pipe(cheerio({
+            run: function($) {
+                $('[fill]').removeAttr('fill');
+                $('[stroke]').removeAttr('stroke');
+                $('[style]').removeAttr('style');
+            },
+            parserOptions: { xmlMode: true }
+        }))
+        // cheerio plugin create unnecessary string '&gt;', so replace it.
+        .pipe(replace('&gt;', '>'))
+        // build svg sprite
+        .pipe(svgSprite({
+            mode: {
+                symbol: {
+                    sprite: "/sprite.svg",
+                    render: {
+                        scss: {
+                            dest: '../../../style/partials/_sprite.scss',
+                            template: "css/_sprite_template.scss"
+                        }
+                    }
+                }
+            }
+        }))
+        .pipe(gulp.dest('./build/svg/'));
+});
+
+gulp.task('svgSprite', ['svgSpriteBuild']);
+
+
+
+gulp.task('build', [
+    'html:build',
+    'js:build',
+    'style:build',
+    'fonts:build',
+    'sprite:build',
+    'svg:build',
+    'image:build'
+]);
+
+
+gulp.task('watch', function() {
+    watch([path.watch.html], function(event, cb) {
+        gulp.start('html:build');
+    });
+    watch([path.watch.style], function(event, cb) {
+        gulp.start('style:build');
+    });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
+    watch([path.watch.img], function(event, cb) {
+        gulp.start('image:build');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('fonts:build');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('sprite:build');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('svg:build');
+    });
+});
+
+
+
+
+gulp.task('default', ['build', 'webserver', 'watch']);
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+//= ../../bower_components/jquery/dist/jquery.min.js
+//= ../../bower_components/owl.carousel/dist/owl.carousel.js
+//= ../../bower_components/nouislider/distribute/nouislider.js
+
+"use strict";
+
